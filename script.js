ac02eb340df7dd5f4bacd6bd345475a62f9e44d0
BaileySweis <97986872+BaileySweis@users.noreply.github.com>
2023-04-03 18:40:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,37 @@
-const express = require('express');
+const http = require('http');
 const sql = require('mssql');
 
-const app = express();
-
-// Define the MSSQL configuration
 const config = {
-  user: 'cis455vgg',
-  password: 'CIS$%%',
-  server: '34.106.124.233',
-  database: 'VGG455',
+  driver: 'ODBC Driver 17 for SQL Server',
+  server: 'ML-RefVm-526995',
+  database: 'GVV2',
   options: {
-    encrypt: true // Use encryption
+    trustedConnection: true
   }
 };
 
-// Define the SQL query to retrieve data from the database
-const query = 'SELECT ProductName, UnitPrice, UnitsInStock, CategoryName FROM `gifted-airport-382516.Categories.Products`, `gifted-airport-382516.Categories.Category` WHERE `gifted-airport-382516.Categories.Products`.CategoryID = `gifted-airport-382516.Categories.Category`.CategoryID';
-
-// Create a route that returns the data in JSON format
-app.get('/data', (req, res) => {
-  sql.connect(config, (err) => {
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  const request = new sql.Request();
+  
+  request.query('SELECT * FROM ', (err, result) => {
     if (err) {
-      console.error(err);
-      res.status(500).send('Internal server error');
+      console.log('Failed to execute query:', err);
       return;
     }
-
-    // Create a request object and execute the SQL query
-    const request = new sql.Request();
-    request.query(query, (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Internal server error');
-        return;
-      }
-
-      // Return the results in JSON format
-      res.json(result.recordset);
-
-      // Close the MSSQL connection
-      sql.close();
-    });
+    
+    res.write(`<pre>${JSON.stringify(result.recordset, null, 2)}</pre>`);
+    res.end();
   });
 });
 
-// Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+sql.connect(config, err => {
+  if (err) {
+    console.log('Failed to connect to database:', err);
+    return;
+  }
+  
+  server.listen(8080, () => {
+    console.log('Server listening on http://localhost:8080/');
+  });
 });
